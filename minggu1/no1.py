083b04ea7d620a3155245be7e1898af8449ad48b
Kadepe77 <kevin.pangemanan20@gmail.com>
2026-09-15 11:37:10 +0700

    Refactor kupon function with default parameters
    Refactor kupon function to handle default parameters and improve logic for calculating rewards.

--- minggu1/no1.py
+++ minggu1/no1.py
@@ -1,2 +1,18 @@
-def kupon(emas, perunggu, perak):
+def kupon(emas=0,perak=0,perunggu=0):
+    if emas < 4:
+        return emas
+    if perak < 3:
+        return perunggu
+    if perunggu < 2:
+        return perunggu
+    emas-=4
+    perak-=3
+    perunggu-=2
     
+    emasB=1+kupon(emas+2)
+    perakB=1+kupon(perak+1)
+    perungguB=1+kupon(perunggu+1)
+
+    return f"Emas: {emasB}, Perak: {perakB}, Perunggu: {perungguB}"
+
+print(kupon(40,30,20))
